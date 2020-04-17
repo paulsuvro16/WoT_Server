73b395b70cefeafcdbeb75d75d0c5974fc132cad
Suvrojoti Paul <paulsuvro16@gmail.com>
2020-04-17 18:28:58 +0530

    initial commit

---
+++ WoTServer/utils/utils.js
@@ -0,0 +1,15 @@
+var resources = require('./../resources/model');
+
+exports.addDevice = function(id, name, description, sensors) {
+  if(!resources.things) {
+    resources.things = {};
+  }
+  resources.things[id] = {'name' : name,
+    'description' : description,
+    'sensors' : sensors
+  }
+};
+
+exports.randomInt = function(low, high) {
+  return Math.floor(Math.random() * (high - low + 1) + low);
+};
